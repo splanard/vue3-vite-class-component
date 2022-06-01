6ed2638f3867a50c655a402aba94e43c362a91a6
splanard <sebastien.planard@gmail.com>
2022-06-01 21:32:15 +0200

    Init Vue project with vue-class-component

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <h1>{{ message }}</h1>
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from "vue-class-component";
+
+export default class HomePage extends Vue {
+  message = "Hello world!";
+}
+</script>
